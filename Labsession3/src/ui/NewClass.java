2e309cd6916be93a014fc20885fc538f3508f93b
Nitya Patel <145049653+NityaPatel01@users.noreply.github.com>
2023-10-04 22:27:15 -0400

    Add files via upload

---
+++ Labsession3/src/ui/NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ui;
+
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author admin
+ */
+public class NewClass extends JFrame {
+    
+    public NewClass() {
+    }
+
+    protected void samplebuttonActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+    
+}
